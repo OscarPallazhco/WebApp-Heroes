c4c1f3edd7f49339b88086ea1ca1cd1df9a2876d
oscarP <edduard.96@gmail.com>
2019-12-24 10:12:59 -0500

    envío de parametros por ruta

---
+++ src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+
+
+const APP_ROUTES: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'heroes', component: HeroesComponent},
+    { path: 'heroes/:nombreHeroe', component: HeroeComponent},
+    { path: '**',pathMatch:'full', redirectTo:'home'}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(APP_ROUTES,{useHash:true})],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
+
+
---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent implements OnInit {
+
+  private heroe:Heroe;
+
+  constructor(private activatedRoute:ActivatedRoute,
+              private _heroesService:HeroesService) {
+    this.activatedRoute.params.subscribe(params =>{
+      let nombreHeroe = params["nombreHeroe"]; 
+      this.heroe = _heroesService.getHeroeByName(nombreHeroe);
+      console.log(this.heroe);
+    })
+  }
+
+  public getHeroe(){
+    return this.heroe;
+  }
+
+  public isMarvel(){return this.heroe.casa=="Marvel"}
+
+  public isDc(){return this.heroe.casa=="DC"}
+
+  ngOnInit() {
+  }
+
+}
